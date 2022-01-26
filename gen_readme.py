77960b60b34e58db7b9694e77b4677fb8c9c852b
ouyen <ouyen@users.noreply.github.com>
2022-01-26 20:58:36 +0800

    2

---
+++ gen_readme.py
@@ -0,0 +1,20 @@
+import os
+from urllib.parse import quote
+
+n=0
+
+with open('README.md','w') as f:
+    f.write('''# 剪报--保存一些看到的有用文章
+
+( 使用singlefile保存网页 )
+
+''')
+    for i in os.listdir("./"):
+        if i[-4:]=='html':
+            n+=1
+            f.write(f"+ [{i[:-5]}](https://ouyen.github.io/saved_html/{quote(i)})\n\n")
+
+
+os.system('git add *')
+os.system('git commit "%s"'%str(n))
+os.system('git push -u origin master')
